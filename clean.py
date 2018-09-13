f24085d9a09c644ada5c372ce98ae1f5c9e28895
Kody <im@kdy.ch>
2018-09-13 15:09:31 +0200

    Update clean.py

--- clean.py
+++ clean.py
@@ -3,29 +3,36 @@ import requests
 
 
 def read_config():
-    with open('config.json') as f:
+    # Load the config.json file in the same directory as this script
+    config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.json')
+    
+    with open(config_file) as f:
         data = json.load(f)
 
     return data
 
 
-def fetch(type, url, token=None, params=None):
+def fetch(req_type, url, token=None, params=None):
+    # Make our own header with a custom UA
     headers = {
         'user-agent': 'PeerTubeCleaner/0.0.1'
     }
 
+    # If we have our token, add the authorization header
     if token:
         headers['authorization'] = 'Bearer ' + token
 
-    if type == 'GET':
+    # Now we do our requests
+    if req_type == 'GET':
         r = requests.get(url, headers=headers)
-    elif type == 'POST':
+    elif req_type == 'POST':
         r = requests.post(url, data=params, headers=headers)
-    elif type == 'DELETE':
+    elif req_type == 'DELETE':
         r = requests.delete(url, headers=headers)
     else:
         raise('No compatible request type used in fetch().')
 
+    # Directly send back the parsed JSON
     return r.json()
 
 
@@ -43,17 +50,23 @@ def login(host, user, pwd):
         'password': pwd
     })
 
-    # Return the token
+    # Return the token directly
     return token['access_token']
 
 
 if __name__ == '__main__':
     print('=== THIS IS A WORK IN PROGRESS ===')
 
+    # Read our config file
     config = read_config()
 
+    # Fetch our user token
     token = login(config['host'], config['username'], config['password'])
+    
+    # List videos
     vids = fetch('GET', config['host'] + '/api/v1/users/me/videos', token)
+    
+    # DEBUG: print the result
     print(vids)
 
     print('=== THIS IS A WORK IN PROGRESS ===')
